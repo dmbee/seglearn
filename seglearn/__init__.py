1757e3135bd13e953ea8fd04a1436e3726b57db8
david <david.mo.burns@gmail.com>
2018-03-13 16:59:49 -0400

    circle ci deps

--- seglearn/__init__.py
+++ seglearn/__init__.py
@@ -1,6 +1,6 @@
 from .pipe import SegPipe
 from .transform import SegmentX, SegmentXY, FeatureRep
-from .split import TemporalKFold
+from .split import TemporalKFold, temporal_split
 from .util import check_ts_data, ts_stats, get_ts_data_parts, make_ts_data
 from .feature_functions import base_features, all_features
 from .datasets import load_watch
@@ -8,4 +8,4 @@ from .base import TS_Data
 
 from . import transform, pipe, util, split, datasets, feature_functions
 
-__all__ = ['TS_Data', 'FeatureRep', 'SegPipe', 'SegmentX', 'SegmentXY', 'TemporalKFold', 'check_ts_data', 'ts_stats', 'make_ts_data','get_ts_data_parts', 'all_features', 'base_features', 'load_watch']
+__all__ = ['TS_Data', 'FeatureRep', 'SegPipe', 'SegmentX', 'SegmentXY', 'TemporalKFold', 'temporal_split', 'check_ts_data', 'ts_stats', 'make_ts_data','get_ts_data_parts', 'all_features', 'base_features', 'load_watch']
