e4b7233744d4bcb68f5b6099464d43fddf4cd5e3
david <david.mo.burns@gmail.com>
2018-03-13 20:38:46 -0400

    removed numpy and scipy

--- setup.py
+++ setup.py
@@ -5,19 +5,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
-
-try:
-    import numpy
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
-
-try:
-    import scipy
-except ImportError:
-    print('scipy is required during installation')
-    sys.exit(1)
-
 setup(name='seglearn',
       version='0.1.1',
       description='Machine Learning with Time Series Segmentation',
