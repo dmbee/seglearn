c8b920ccc95d45cd81902d4f52e0af171e94f30b
david <david.mo.burns@gmail.com>
2018-07-24 15:58:05 -0400

    added version and author to init

--- seglearn/__init__.py
+++ seglearn/__init__.py
@@ -12,3 +12,6 @@ from .base import TS_Data
 from . import transform, pipe, util, split, datasets, feature_functions
 
 __all__ = ['TS_Data', 'FeatureRep', 'PadTrunc', 'SegPipe', 'SegmentX', 'SegmentXY','SegmentXYForecast', 'TemporalKFold', 'temporal_split', 'check_ts_data', 'ts_stats', 'make_ts_data','get_ts_data_parts', 'all_features', 'base_features', 'load_watch']
+
+__author__ = 'David Burns david.mo.burns@gmail.com'
+__version__ = '0.2.0'
