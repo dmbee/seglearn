03f0990db9a88d03c61166cec92bbe8547691945
david <david.mo.burns@gmail.com>
2022-08-26 10:41:43 -0700

    travis update

--- setup.py
+++ setup.py
@@ -35,7 +35,8 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Operating System :: Unix',
                'Operating System :: MacOS',
                'Programming Language :: Python :: 3.5',
-               'Programming Language :: Python :: 3.6']
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.8']
 EXTRAS_REQUIRE = {
     'tests': [
         'pytest',
